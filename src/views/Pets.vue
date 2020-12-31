f6ce46fc05917f9aadd367adff98922492efc7cd
Duygu Temur <duygutemur@archis.company>
2020-12-31 18:05:05 +0300

    create app basic

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home">
-   <h1>Adopt best friend</h1>
+  <div>
+      pet
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
+
 }
 </script>
+
+<style>
+
+</style>
